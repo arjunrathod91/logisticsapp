67140171b5a66da2bb54ffb59615b136f9078634
ArjunRathod <138087179+arjunrathod91@users.noreply.github.com>
2024-01-12 11:22:21 +0530

    Update NearbyHelpers.jsx

--- src/components/User/NearbyHelpers.jsx
+++ src/components/User/NearbyHelpers.jsx
@@ -47,25 +47,7 @@ function NearbyHelpers() {
                 <p className="card-text">Location:{item.address}</p>
               </div>
             </div>
-    <div className="container pt-2">
-      <Header />
-      <h4>Nearby Helpers</h4>
-      <div className="row">
-        {cardData.map((card) => (
-          <div key={card.id} className="col-lg-4 col-md-6 mb-4">
-            <Link to="/volunteerprofile" style={{ textDecoration: 'none' }}>
-              <div className="card custom-card">
-                <div className="card-body " style={{backgroundColor:"rgb(41, 169, 204)"}}>
-                  <div className="circle custom-circle"></div>
-                  <h5 className="card-title" style={{ fontSize: '1.2rem', marginBottom: '0.5rem' }}>Name: {card.Name}</h5>
-                  <p className="card-text" style={{ fontSize: '1rem', marginBottom: '0.5rem' }}>Service: {card.Service}</p>
-                  <p className="card-text" style={{ fontSize: '1rem', marginBottom: '0.5rem' }}>Location: {card.Location}</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
+            </div>
     </div>
   );
 }
