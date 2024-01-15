5ecd88ad359d7d9c102eb556cca5244a88cee04d
ArjunRathod <138087179+arjunrathod91@users.noreply.github.com>
2024-01-15 12:46:25 +0530

    Update Header.jsx

--- src/components/User/Header.jsx
+++ src/components/User/Header.jsx
@@ -1,15 +1,7 @@
 import React, { useContext, useState } from "react";
-import { Nav, NavItem } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faSearch,
-  faHome,
-  faUserCircle,
-  faEnvelope,
-  faExclamationCircle,
   faBell,
-  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { MyContext } from "../../Contexts/AllContext";
 
