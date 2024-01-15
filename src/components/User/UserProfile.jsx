cdcd7ec29e76c98f4ba5dfc23d07f3493dec4ad7
ArjunRathod <138087179+arjunrathod91@users.noreply.github.com>
2024-01-15 11:32:05 +0530

    Update UserProfile.jsx

--- src/components/User/UserProfile.jsx
+++ src/components/User/UserProfile.jsx
@@ -16,22 +16,22 @@ const UserProfile = () => {
   return (
     <div>
         <Header/>
-        {userData ? <div className='container mt-12'>
+             <div className='container mt-12'>
                 <div className='center-flex gap-5'>
                     <img src="https://th.bing.com/th?id=ORMS.d371f0730994d45f73252117479664bb&pid=Wdp&w=300&h=156&qlt=90&c=1&rs=1&dpr=1.25&p=0" alt="" style={{borderRadius:'50%',height:'80px',width:'80px'}} />
                     <div className='d-flex flex-col'>
-                        <p>Name : {userData.username}</p>
-                        <p>Email: {userData.email}</p>
+                        <p>Name : {userData ? userData.username : ''}</p>
+                        <p>Email: {userData ? userData.email : ''}</p>
                     </div>
                 </div>
                 <div className='container-fluid center-flex flex-col mt-10'>
                     <div>
-                        <strong>Location : {userData.location}</strong>
-                        <p><FontAwesomeIcon icon={faPhone} className='mr-2'/> Contact : {userData.contact}</p>
+                        <strong>Location : {userData ? userData.location : ''}</strong>
+                        <p><FontAwesomeIcon icon={faPhone} className='mr-2'/> Contact : {userData ? userData.contact : ''}</p>
                         <Link to="/editinfo"><button className='' style={{backgroundColor:"teal",color:"white",borderRadius:"10px"}}>Edit Information</button></Link>
                     </div>
                 </div>
-            </div> : '' }
+            </div>
             <Footer/>
         </div>
   )
