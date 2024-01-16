41e08a4c81f26c32561f10f9d047ff5f0478ccba
ArjunRathod <138087179+arjunrathod91@users.noreply.github.com>
2024-01-16 12:22:07 +0530

    Update Mainpage.jsx

--- src/components/Volunteer/Mainpage.jsx
+++ src/components/Volunteer/Mainpage.jsx
@@ -8,7 +8,7 @@ import { MyContext } from '../../Contexts/AllContext'
 const Mainpage = () => {
 
   const [request,setRequest] = useState([])
-  const {voldata,setVolData} = useContext(MyContext)
+  const {voldata,setVolData,userData,setUserData} = useContext(MyContext)
 
   const navigate = useNavigate()
 
@@ -17,37 +17,41 @@ const Mainpage = () => {
     const userId = item._id
     axios.get(`http://localhost:3002/userDetail/${userId}`)
     .then(result=>{
-      console.log(result.data)
+      setUserData(result.data)
+      navigate('/user-info')
     })
     .catch(err=>console.log(err))
   }
 
   useEffect(()=>{
-    axios.get('http://localhost:3002/newRequest')
+    const area = voldata.area
+    axios.get('http://localhost:3002/newRequest',{
+      params:{area}
+    })
     .then((data)=>{
       setRequest(data.data)})
     .catch(err=>console.log(err))
-  },[voldata])
+  },[request])
 
   return (
     <div className="container">
       <VolHeader />
       <p className='ml-8 mt-5' style={{ fontSize: "20px", fontWeight: "bold" }}>Need a Help !</p>
       <div className='d-flex container-fluid flex-column  align-items-center'>
-        {request ? request.map((item)=>(
+        { request ? request.map((item)=>(
           <div className='request-box mt-1 d-flex' onClick={(e)=>userInfo(e,item)}>
           <div className='left w-[30%] h-[100%] center-flex' >
-            <img src="https://th.bing.com/th?id=ORMS.d371f0730994d45f73252117479664bb&pid=Wdp&w=300&h=156&qlt=90&c=1&rs=1&dpr=1.25&p=0" alt="" className='w-[60px] h-[60px]' style={{backgroundColor:"red",borderRadius:'50%'}}/>
+            <img src="https://th.bing.com/th?id=ORMS.d371f0730994d45f73252117479664bb&pid=Wdp&w=300&h=156&qlt=90&c=1&rs=1&dpr=1.25&p=0" alt="" className='w-[60px] h-[60px]' style={{borderRadius:'50%'}}/>
           </div>
           <div className='right w-[70%] h-[100%] flex-column'>
-          <p>{item.name}</p>
-          <p>{item.help}</p>
-          <p>{item.location}</p>
+          <p><span className='fw-bold'>Name :</span> {item.name}</p>
+          <p><span className='fw-bold'>Help :</span> {item.help}</p>
+          <p><span className='fw-bold'>Location :</span> {item.location}</p>
           </div>
         </div>
         )) : ''}
       </div>
-    <VolFooter/>
+      <VolFooter/>
     </div>
   )
 }
