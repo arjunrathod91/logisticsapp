618d6dcc8271a5bfa2ee0c66063fa323231be86f
ArjunRathod <138087179+arjunrathod91@users.noreply.github.com>
2024-01-16 12:21:28 +0530

    Update UserInfo.jsx

--- src/components/Volunteer/UserInfo.jsx
+++ src/components/Volunteer/UserInfo.jsx
@@ -1,32 +1,30 @@
 import React, { useContext } from 'react'
-import VolHeader from './VolHeader'
 import VolFooter from './VolFooter'
+import VolHeader from './VolHeader'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faPhone} from '@fortawesome/free-solid-svg-icons'
 import { MyContext } from '../../Contexts/AllContext'
 
 
 const UserInfo = () => {
+    const {userData,setUserData} = useContext(MyContext)
     return (
         <div>
             <VolHeader />
-            {userData ? <div className='container mt-12'>
+            { userData ? <div className='container mt-12'>
                 <div className='center-flex gap-5'>
-                    <img src="https://th.bing.com/th?id=ORMS.d371f0730994d45f73252117479664bb&pid=Wdp&w=300&h=156&qlt=90&c=1&rs=1&dpr=1.25&p=0" alt="" className=' userinfo-img' />
+                    <img src="https://th.bing.com/th?id=ORMS.d371f0730994d45f73252117479664bb&pid=Wdp&w=300&h=156&qlt=90&c=1&rs=1&dpr=1.25&p=0" alt="" className='w-[100px] h-[100px]' />
                     <div className='start-flex flex-col'>
-                        <p>Name : {userData.username}</p>+
-                        <p>Ask for : {userData.help}</p>
+                        <p><span className='fw-bold'>Name :</span>{userData.name}</p>
+                        <p><span className='fw-bold'>Help :</span>{userData.help}</p>
                     </div>
                 </div>
-                <div className='container-fluid center-flex flex-col mt-3'>
-                        <strong>Location :</strong> {userData.location}
-                    <div>
+                <div className='container-fluid center-flex flex-col mt-10'>
+                        <strong><span className='fw-bold mt-3 mb-3'>Location :</span>{userData.location}</strong>
                         <p onClick={''}><FontAwesomeIcon icon={faPhone} className='mr-2'/> {userData.contact}</p>
-                    </div>
-                    
                 </div>
             </div> : ''}
-                <VolFooter/>
+            <VolFooter/>
         </div>
     )
 }
