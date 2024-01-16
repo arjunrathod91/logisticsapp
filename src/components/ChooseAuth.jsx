2e767a8b1249e5d17a4c82a96ae7627fc6d1f6c8
ArjunRathod <138087179+arjunrathod91@users.noreply.github.com>
2024-01-16 14:26:37 +0530

    Update ChooseAuth.jsx

--- src/components/ChooseAuth.jsx
+++ src/components/ChooseAuth.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faHandHoldingHeart } from '@fortawesome/free-solid-svg-icons';
-import logo from '../assets/logo.png'
+import logo from '../../assets/logo.png'
 const ChooseAuth = () => {
   return (
     <>
